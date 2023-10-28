74c9474841aba0675d837274f73a0e96444d0609
a.ito <10333049+BoxPistols@users.noreply.github.com>
2023-10-29 05:36:57 +0900

    markdown blog base

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  // raw-loaderの読み込み設定
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
+    return config
+  },
+}
